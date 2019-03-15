80e3a061a547b3f3e8d7438989bf93c56f22e5a0
Tanja Stanic <tanja_stanic@uns.ac.rs>
2019-03-15 16:34:24 +0100

    First commit - prazna maven aplikacija

---
+++ MegaTravel/src/main/java/project/besp/MegaTravel/MegaTravelApplication.java
@@ -0,0 +1,13 @@
+package project.besp.MegaTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaTravelApplication.class, args);
+	}
+
+}
