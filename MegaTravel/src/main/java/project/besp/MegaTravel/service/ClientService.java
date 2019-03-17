b9911d24a5987af2bb61efd14335725e041df627
bojan681 <46452451+bojan681@users.noreply.github.com>
2019-03-17 11:42:08 +0100

    Uradjeni servisi - client i employee

---
+++ MegaTravel/src/main/java/project/besp/MegaTravel/service/ClientService.java
@@ -0,0 +1,21 @@
+package project.besp.MegaTravel.service;
+
+import org.springframework.stereotype.Service;
+
+import project.besp.MegaTravel.model.Client;
+@Service
+public interface ClientService {
+	
+	Client registracijaKlijenta(Client k);
+	
+	Client registracijaKlijentaF(Client k);
+
+	Client preuzmiKlijenta(Long id);
+
+
+
+}
+
+
+
+	
---
+++ MegaTravel/src/main/java/project/besp/MegaTravel/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package project.besp.MegaTravel.service;
+
+import org.springframework.stereotype.Service;
+
+
+import project.besp.MegaTravel.model.Employee;
+@Service
+public interface EmployeeService {
+	
+	Employee registracijaZaposlenog(Employee z);
+
+	Employee preuzmiZaposlenog(Long id);
+
+}
