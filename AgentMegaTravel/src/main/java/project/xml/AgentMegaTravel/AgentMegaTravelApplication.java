2a45895844b521179fa2f2b299a2a01e7c881903
Sara Celik <sara.celik@live.com>
2019-03-27 21:14:43 +0100

    agent modul

---
+++ AgentMegaTravel/src/main/java/project/xml/AgentMegaTravel/AgentMegaTravelApplication.java
@@ -0,0 +1,14 @@
+package project.xml.AgentMegaTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentMegaTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentMegaTravelApplication.class, args);
+		System.out.println("HELLO");
+	}
+
+}
---
+++ AgentMegaTravel/src/test/java/project/xml/AgentMegaTravel/AgentMegaTravelApplicationTests.java
@@ -0,0 +1,16 @@
+package project.xml.AgentMegaTravel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AgentMegaTravelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
