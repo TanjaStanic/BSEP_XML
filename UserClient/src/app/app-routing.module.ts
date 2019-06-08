798c13edabe50a7a6147c737c04b310495397b9b
bojan681 <46452451+bojan681@users.noreply.github.com>
2019-06-09 00:51:16 +0200

    User Client

--- UserClient/src/app/app-routing.module.ts
+++ UserClient/src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AddAgentComponent } from './add-agent/add-agent.component';
+import { CommentsComponent } from './comments/comments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	
+	{
+      path: 'login',
+      component: LoginComponent
+    },
+    
+    {
+      path: 'register',
+      component: RegisterComponent
+    },
+    {
+      path: 'homePage',
+      component: HomePageComponent
+    },
+    {
+      path: 'addAgents',
+      component: AddAgentComponent
+    },
+    {
+      path: 'comments',
+      component: CommentsComponent
+    }
+	
+	];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- UserClient/src/app/app.module.ts
+++ UserClient/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+import { HomePageComponent } from './home-page/home-page.component';
+import { AddAgentComponent } from './add-agent/add-agent.component';
+import { CommentsComponent } from './comments/comments.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomePageComponent,
+    AddAgentComponent,
+    CommentsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
