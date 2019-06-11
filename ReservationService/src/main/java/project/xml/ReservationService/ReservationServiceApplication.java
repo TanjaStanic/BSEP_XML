d458b8979b0cbe9b200da4fa4d878b5e2d1ab52e
Sara Celik <sara.celik@live.com>
2019-06-11 20:52:14 +0200

    aaa

--- ReservationService/src/main/java/project/xml/ReservationService/ReservationServiceApplication.java
+++ ReservationService/src/main/java/project/xml/ReservationService/ReservationServiceApplication.java
@@ -10,6 +10,6 @@ public class ReservationServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
 	}
-	//aaaaaa
+	//aaaaaaaa
 
 }
